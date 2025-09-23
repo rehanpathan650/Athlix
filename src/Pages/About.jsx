9d64fd1846ddec54dcb04e874a36aa1e296ac3ec
Rehan Pathan <pathanrehan650@gmail.com>
2025-09-23 18:09:30 +0530

    second commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen px-4'>
+       <Navbar />
+       <br />
+       <div>About Athlix</div>
+       <hr />
+       <br />
+       <p>At Athlix, we believe sportswear should do more than just look good — it should empower you to perform at your best. From premium shoes to versatile activewear, every product is designed with comfort, durability, and style in mind.
+        Our mission is simple: to help athletes, fitness lovers, and everyday movers feel confident in what they wear, whether on the track, in the gym, or out on the streets. With Athlix, you don’t just wear sportswear — you wear motivation.
+       </p>
+    </div>
   )
 }
 
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function Product() {
   return (
-    <div>Product</div>
+    <div className='min-h-screen px-4'>
+      <Navbar />
+    </div>
   )
 }
 
